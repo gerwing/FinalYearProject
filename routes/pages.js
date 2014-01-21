0270da8c06ac9d3ff68992f003fc17692b522cc9
Gerwin Glorieux <gerwin.glorieux@kcl.ac.uk>
2014-01-21 20:19:22 +0000

    Added Login and Register Routes in pages.js route file

--- routes/pages.js
+++ routes/pages.js
@@ -7,6 +7,16 @@ module.exports = function(app) {
 
     //GET INDEX
     app.get('/', function(req, res){
-            res.render('index', { title: 'Express' });
+        res.render('index', { title: 'Express' });
+    });
+
+    //LOGIN TEACHER
+    app.post('/teacher/login', function(req,res) {
+        //Login Teacher
+    });
+
+    //REGISTER TEACHER
+    app.post('/teacher/register', function(req,res) {
+       //Register Teacher
     });
 };
