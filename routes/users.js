1599373d34b71161f5bbf062dc3ac0ab9de365ea
Gerwin Glorieux <gerwin.glorieux@kcl.ac.uk>
2014-01-23 18:40:49 +0000

    Implemented teacher registration API using a post method to /teacher/register

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,22 @@
 /*
  * User routes
  */
+var User = require('../data/models/users');
 
 module.exports = function(app) {
 
     //REGISTER TEACHER
-    app.post('/teacher/register', function(req,res) {
+    app.post('/teacher/register', function(req,res,next) {
+        //Mark as teacher
+        var newTeacher = req.body;
+        newTeacher.isTeacher = true;
         //Register Teacher
+        User.create(newTeacher, function(err) {
+            if(err) {
+                return next(err);
+            }
+            res.send('Success', 200);
+        });
     });
 
     //UPDATE TEACHER
