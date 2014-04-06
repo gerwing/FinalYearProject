fa4afc0cfe55ddc27b3fdf8ebf3b912e096e2ca3
Gerwin Glorieux <gerwin.glorieux@kcl.ac.uk>
2014-04-06 22:02:01 +0100

    Changed the client side socket configuration to use the openshift socket connection

--- webapp/app/scripts/services/socket.js
+++ webapp/app/scripts/services/socket.js
@@ -2,6 +2,6 @@
 
 angular.module('voteApp')
     .factory('SocketIO', function () {
-        var socket = io.connect();
+        var socket = io.connect('ws://vote-gaving.rhcloud.com:8000/');
         return socket;
     });
