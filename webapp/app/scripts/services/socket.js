3957698ba27b004fec6129180b026fd45084a995
Gerwin Glorieux <gerwin.glorieux@kcl.ac.uk>
2014-04-08 15:50:04 +0100

    Changed port number of websockets to 8443 for SSL

--- webapp/app/scripts/services/socket.js
+++ webapp/app/scripts/services/socket.js
@@ -2,6 +2,6 @@
 
 angular.module('voteApp')
     .factory('SocketIO', function () {
-        var socket = io.connect('ws://vote-gaving.rhcloud.com:8000/');
+        var socket = io.connect('ws://vote-gaving.rhcloud.com:8443/');
         return socket;
     });
