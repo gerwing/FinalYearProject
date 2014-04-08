7ec3eff915ade4c6b80de0bab26e58acc0b4d842
Gerwin Glorieux <gerwin.glorieux@kcl.ac.uk>
2014-04-08 19:57:52 +0100

    Added Live results during lectures, teachers can now follow answers as they come in on a bar chart

--- webapp/app/scripts/controllers/teacher/liveLecture.js
+++ webapp/app/scripts/controllers/teacher/liveLecture.js
@@ -7,6 +7,8 @@ angular.module('voteApp')
             if(!Authentication.verifyTeacher()){
                 return;
             }
+
+            /**INITIALIZE SOCKET.IO*/
             //Reconnect socket in case teacher had left the lecture
             socket.socket.connect();
             //Join lecture room
@@ -25,8 +27,9 @@ angular.module('voteApp')
                 $scope.$apply();
             });
             //New answer received
-            socket.on('newAnswer', function() {
+            socket.on('newAnswer', function(data) {
                 $scope.answered += 1;
+                addAnswerChart(data); //Add answer to bar chart
                 $scope.$apply();
             });
             //Close Connection when leaving page
@@ -44,12 +47,32 @@ angular.module('voteApp')
                 Lecture.update({id:$scope.lecture._id}, {isLive:false});
             }
 
+            /**INITIALIZE SCOPE*/
             //Get Lecture Data
             $scope.lecture = Lecture.get({id: $routeParams.id}, function(){
+                //Initialize scope variables
                 $scope.question = $scope.lecture.questions[0];
                 $scope.currentQuestion = 0; //Index of current question being shown
                 $scope.lastAnswered=0; //Question that was last answered
+                resetChartData(); //initialize pie chart data
             });
+            //Bar Chart Data
+            $scope.chartData = { //Bar chart Data
+                labels : [],
+                answers: [],
+                datasets : [{
+                    fillColor : "rgba(151,187,205,0.5)",
+                    strokeColor : "rgba(151,187,205,1)",
+                    data:[]
+                }]
+            };
+            //Bar Chart Options
+            $scope.chartOptions = {
+                scaleShowGridLines: true,
+                scaleShowLabels: false
+            };
+
+            /**SCOPE METHODS*/
             $scope.nextQuestion = function() {
                 $scope.resultview = false; //turn of resultview
                 $scope.currentQuestion += 1;
@@ -58,7 +81,13 @@ angular.module('voteApp')
                 if($scope.lastAnswered<$scope.currentQuestion) {
                     $scope.lastAnswered = $scope.currentQuestion; //Set new last answered question
                 }
+                resetChartData(); //Reset chart data using new question
                 socket.emit('changeQuestion', $scope.currentQuestion);
+            };
+            $scope.gotoQuestion = function(index) {
+                $scope.currentQuestion = index;
+                $scope.question = $scope.lecture.questions[$scope.currentQuestion];
+                resetChartData(); //Reset chart data using new question
             };
             $scope.showResults = function() {
                 $scope.resultview = true; //turn on resultview
@@ -76,9 +105,25 @@ angular.module('voteApp')
                 //End Lecture
                 $scope.finished = true;
                 socket.emit('finished');
+            };
+
+            /**OTHER METHODS*/
+            //Method that resets the data used for the pie chart
+            function resetChartData() {
+                $scope.chartData.labels = [];
+                $scope.chartData.answers = []; //Seperate answer array for quickly finding answer in chart
+                $scope.chartData.datasets[0].data = [];
+                //Add labels and default data to chart data based on current question
+                for(var i=0;i<$scope.question.answers.length;i++) {
+                    $scope.chartData.labels.push("A."+(i+1));
+                    $scope.chartData.answers.push($scope.question.answers[i].answer);
+                    $scope.chartData.datasets[0].data.push(0);
+                }
             }
-            $scope.gotoQuestion = function(index) {
-                $scope.currentQuestion = index;
-                $scope.question = $scope.lecture.questions[$scope.currentQuestion];
+            //Method that registers an answer in the barchart
+            function addAnswerChart(answer) {
+                //Register answer in bar chart
+                var index = $scope.chartData.answers.indexOf(answer);
+                $scope.chartData.datasets[0].data[index] += 1;
             }
     }]);
